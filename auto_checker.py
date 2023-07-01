6781081a0c5240e033660fb0b1b7c4314ab2ac1a
matteo-luraghi <matteoluraghi50@gmail.com>
2023-07-01 17:00:34 +0200

    changed readme

--- auto_checker.py
+++ auto_checker.py
@@ -3,7 +3,7 @@ import os, time
 i = 1
 while(i < 101):
     print(f"Running script on file #{i}")
-    os.system(f"/usr/bin/time ./main-hash < ./open/open_{i}.txt > out.txt")
+    os.system(f"/usr/bin/time ./30 < ./open/open_{i}.txt > out.txt")
     time.sleep(1)
     print("Differences:")
     os.system(f"diff ./open/open_{i}.output.txt ./out.txt")
