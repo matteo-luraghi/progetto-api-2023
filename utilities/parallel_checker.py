446091d5470d7aa224cf00d9c067afa613b60864
matteo-luraghi <matteoluraghi50@gmail.com>
2023-08-23 19:04:36 +0200

    added parallel checker

---
+++ utilities/parallel_checker.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+l=[]
+for i in range(1,112):
+    print("Running on file:",i)
+    s="./open/open_"+str(i)+".txt"
+    input=open(s,"r")
+    output=open(f"./out/output{i}.txt","w")
+    proc=subprocess.run("./30",stdin=input,stdout=output)
+    output.close()
+
+    s="./open/open_"+str(i)+".output.txt"
+    proc=subprocess.run(["diff",s,f"./out/output{i}.txt"],capture_output=True)
+    if proc.stdout.decode() != "":
+        l.append(i)
+    #os.system("clear")
+
+print(l)
