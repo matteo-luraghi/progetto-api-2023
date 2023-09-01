5fad71cb33be1b22086e68d56f1b2cad79028f1d
matteo-luraghi <matteoluraghi50@gmail.com>
2023-09-01 20:45:32 +0200

    using heapsort instead of quicksort

--- 30.c
+++ 30.c
@@ -94,28 +94,42 @@ nodo_lista_t* list_remove_tail(lista_t* l) {
     l->tail->next = NULL;
     return tail;
 }
+void max_heapify(int* A, int i, int heap_size) {
+    int l = 2*i;
+    int r = (2*i)+1;
+    int max;
+    if(l<=heap_size && A[l] > A[i]) {
+        max = l;
+    }
+    else {
+        max = i;
+    }
+    if(r<=heap_size && A[r] > A[max]) {
+        max = r;
+    }
+    if(max != i) {
+        int temp = A[i];
+        A[i] = A[max];
+        A[max] = temp;
+        max_heapify(A, max, heap_size);
+    }
+}
 
-void quick_sort(int* A, int p, int r) {
-    if(p < r) {
-        int x = A[r];
-        int i = p - 1;
-        int temp;
-        for(int j=p; j<r;j++) {
-            if(A[j] <= x) {
-                i++;
-                temp = A[i];
-                A[i] = A[j];
-                A[j] = temp;
-            }
-        }
-        temp = A[i+1];
-        A[i+1] = A[r];
-        A[r] = temp;
+void build_max_heap(int* A, int len) {
+    for(int i = len/2; i>=0; i--) {
+        max_heapify(A, i, len);
+    }
+}
 
-        int q = i+1;
-
-        quick_sort(A, p, q-1);
-        quick_sort(A, q+1, r);
+void heapsort(int* A, int len) {
+    build_max_heap(A,len);
+    int heap_size = len;
+    for(int i=len; i>=1; i--) {
+        int temp = A[0];
+        A[0] = A[i];
+        A[i] = temp;
+        heap_size--;
+        max_heapify(A, 0, heap_size);
     }
 }
 
@@ -751,7 +765,7 @@ int main() {
             int nodi_temp = nodi_utili[0];
             for(int h=1; h<=nodi_utili_len;h++) {
                 if(nodi_temp > nodi_utili[h]) {
-                    quick_sort(nodi_utili, 0, nodi_utili_len-1);
+                    heapsort(nodi_utili, nodi_utili_len-1);
                     break;
                 }
                 nodi_temp = nodi_utili[h];
